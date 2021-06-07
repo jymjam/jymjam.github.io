f2471942d90772057967339d945fa7fc09b7e7a4
white-panda01 <hars0101@gmail.com>
2021-06-07 08:24:24 +0400

    boomerang update 0.2

--- rendum.js
+++ rendum.js
@@ -2,7 +2,7 @@ const img = document.getElementById('banner')
 const ran = Math.floor((Math.random() * 10) + 1)
 
 if(ran % 2 == 0){
-    img.src = './assets/katz.gif'
+    img.src = './assets/katz-boomeranged.gif'
 }else{
     img.src = './assets/minecraft.gif'
 }
